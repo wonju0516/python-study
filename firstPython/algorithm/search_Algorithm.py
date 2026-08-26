db358bce9455588ba29a0aaa7c7111832ce5aec4
wonju0516 <dnjswn0516@gmail.com>
2026-08-26 19:32:43 +0900

    feat: 이진 탐색과 bisect 모듈 학습 코드 추가

--- firstPython/algorithm/search_Algorithm.py
+++ firstPython/algorithm/search_Algorithm.py
@@ -26,7 +26,7 @@ print(letter in "apple")
 # ? 데이터가 정렬되어 있다면 모든 요소를 검사하는 선형 탐색보다 이진 탐색을 사용할 수 있음
 # * 이진 탐색 (Binary Search)
 def binary_search(a_list, n):
-    first = 0
+    first = 0  # * 인덱스
     last = len(a_list) - 1
     while last >= first:
         mid = (first + last) // 2
@@ -38,3 +38,24 @@ def binary_search(a_list, n):
             else:
                 first = mid + 1
         return False
+
+
+# ? else: 이런식으로 작성되어 있는 그냥 elif n < a_list[mid]: -> 이런식으로 작성해도 댐!!
+
+# ! 이진 탐색도 파이썬에 내장 모듈을 통해 구현할 수 있음
+# ! bisect 모듈에 들어 있는 bisect_left -> 정렬된 리스트에 대상이 없는 경우 존재했다면 있었을 인덱스를 반환한다, 았으면 그 인덱스 반환함
+from bisect import bisect_left
+
+sorted_fruits = ["apple", "banana", "orange", "plum"]
+# ! bisect_left만 실행하면 결과값이 함수 실행 후 사라져버려서 화면엔 아무것도 안 뜬다.
+# ! 화면에 결과를 보려면 반드시 print()로 감싸야 한다.
+print(bisect_left(sorted_fruits, "fruit"))
+
+# ! bisect_left는 찾으려는 요소가 리스트에 있는지 확인하기 위해서 그 인덱스가 먼저 있는지 체크하는 코드로 작성해야 함
+
+
+def binary_search1(an_iterable, target):
+    index = bisect_left(an_iterable, target)
+    if index < len(an_iterable) and an_iterable[index] == target:
+        return True
+    return False
