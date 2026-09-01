8d4b587453ece12e75e7ad5ea063a549245d5839
wonju0516 <dnjswn0516@gmail.com>
2026-09-01 16:57:07 +0900

    feat: 수학 연산 예제 추가

---
+++ firstPython/python_basics/mathmatical_operation.py
@@ -0,0 +1,22 @@
+print(3 + 1)
+
+print(3 - 1)
+
+print(3 * 1)
+
+# * float
+print(3 / 2)
+
+# * float division ->  몫만 가져오기
+print(3 // 2)
+
+# * Exponents -> 지수
+print(3**3)
+
+# * Modulo -> 나머지
+print(60 % 13)
+print(12.5 % 5.5)  # ! 소수점도 가능!!
+
+sum = 0
+sum += 1
+print(sum)
