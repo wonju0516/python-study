f46b1aa5234d27d2ebd64152cdbfe7a53f948397
wonju0516 <dnjswn0516@gmail.com>
2026-09-04 18:15:17 +0900

    feat: 로봇 게임

---
+++ firstPython/python_basics/loop_function/robot_game.py
@@ -0,0 +1,13 @@
+# robot game (function)
+
+
+def my_func(country):
+    print(f"Hello world, {country}")
+
+
+my_func("korea")
+
+# * https://reeborg.ca
+
+# * indentation -> 들여쓰기
+## * tab vs space -> 요즘은 space를 쓰는 추세라고 하긴함
